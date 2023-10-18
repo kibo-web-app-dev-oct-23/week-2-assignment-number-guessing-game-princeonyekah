12dd9718af5849f6b18ae92b038e20edaf7c2207
Princewill <princewill.onyekah@kibo.school>
2023-10-18 09:47:25 +0100

    first

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return render_template("index.html")
+
+@app.route('/game', methods=["GET"])
+def game():
+    number = []
+    for num in range(1,101):
+        number.append(num)
+
+    return render_template('game.html', number = number)
+
+
+@app.route('/guess/<int:number>', methods=["GET"])
+def show_number(number):
+    return render_template('result.html', number = number)
+
+if __name__ == '__main__':
+    # Run the Flask web server with debugging mode turned on
+    app.run(debug=True)
